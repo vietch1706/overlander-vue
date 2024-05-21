a21be7dfe8db5278b81f3bda35df99a971492797
Viet <cuhoaiviet1415@gmail.com>
2024-05-21 12:06:02 +0700

    feat: add axios

--- src/components/ResisterPage/FormComponent.vue
+++ src/components/ResisterPage/FormComponent.vue
@@ -44,13 +44,14 @@
         </template>
         <div class="col-sm-4 pt-2 phone-code-field">
           <el-form-item prop="phoneCode">
-            <el-select v-model="data.user.phoneCode" placeholder="Code">
+            <el-select v-model="data.user.phoneCode" placeholder="Select">
               <el-option
-                v-for="value in data.phoneCode"
-                :key="value.id"
-                :value="value.id"
-                :label="value.iso3"
-              />
+                v-for="item in data.country"
+                :key="item.id"
+                :label="item.id"
+                :value="item.phonecode"
+              >
+              </el-option>
             </el-select>
           </el-form-item>
         </div>
@@ -95,15 +96,16 @@
               </label>
             </template>
             <el-select
-              v-model="data.user.region"
+              v-model="data.user.country"
               filterable
               class="overwrite-form-item-register"
+              placeholder="Select Country"
             >
               <el-option
-                v-for="value in data.country"
-                :key="value.id"
-                :value="value.id"
-                :label="value.name"
+                v-for="item in data.country"
+                :key="item.id"
+                :label="item.id"
+                :value="item.name"
               ></el-option>
             </el-select>
           </el-form-item>
@@ -161,7 +163,7 @@
           >
         </template>
         <div class="col sp-2 pt-3">
-          <el-form-item prop="year">
+          <el-form-item prop="gender">
             <el-radio
               v-model="gender"
               label="male"
@@ -172,7 +174,7 @@
           </el-form-item>
         </div>
         <div class="col ps-2 pt-3">
-          <el-form-item prop="month">
+          <el-form-item prop="gender">
             <el-radio
               v-model="gender"
               label="female"
@@ -211,7 +213,7 @@
         <div class="col sp-2 check-box">
           <template>
             <!-- `checked` should be true or false -->
-            <el-checkbox v-model="checked"
+            <el-checkbox v-model="checkedNews"
               >I would like to receive email about news and
               promotion</el-checkbox
             >
@@ -222,7 +224,7 @@
         <div class="col sp-2 check-box">
           <template>
             <!-- `checked` should be true or false -->
-            <el-checkbox v-model="checked"
+            <el-checkbox v-model="checkedPromotion"
               >I would like to receive promotion booklet</el-checkbox
             >
           </template>
@@ -248,22 +250,24 @@ export default {
   name: "app",
   data() {
     return {
-      checked: true,
+      checkedNews: false,
+      checkedPromotion: false,
       gender: "",
       url: "http://anywhere.com",
       data: {
+        country: [],
         user: {
           firstName: "",
           lastName: "",
-          phoneCode: "",
+          phoneCode: null,
           phoneNumber: "",
           password: "",
-          country: "",
+          country: null,
           email: "",
           year: "",
           month: "",
           gender: "",
-          interests: "",
+          interests: null,
         },
       },
       rules: {
@@ -291,6 +295,18 @@ export default {
             trigger: "blur",
           },
         ],
+        year: [
+          {
+            min: 4,
+            message: "Too short year",
+            trigger: "blur",
+          },
+          {
+            max: 4,
+            message: "Too long year",
+            trigger: "blur",
+          },
+        ],
       },
     };
   },
@@ -312,7 +328,11 @@ export default {
       axios
         .get("general/phonecode/get")
         .then((result) => {
-          this.data.user.phoneCode = result.data;
+          this.data.country = result.data;
+          for (let keys in result.data) {
+            this.data.country[keys]["phonecode"] =
+              "+ " + result.data[keys]["phonecode"];
+          }
         })
         .catch((error) => {
           console.log(error);
@@ -326,121 +346,5 @@ export default {
 </script>
 
 <style lang="scss">
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-.form-container {
-  .el-form-item__content {
-    line-height: unset;
-  }
-  padding: 0 3.5rem;
-  .overwrite-label-register {
-    font-weight: 600;
-    font-size: 18px;
-    line-height: 18px;
-    color: #3c4a5d;
-    .optional {
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 18px;
-      color: #6d7786;
-    }
-    .required {
-      display: inline-block;
-      color: #e22b30;
-      font-size: 20px;
-    }
-  }
-  .append-slot {
-    .el-input__inner {
-      border-right: none;
-    }
-  }
-
-  .phone-code-field {
-    .el-input__inner {
-      background: #e7e9eb;
-      border-color: #ced2d7;
-      color: #0b1d35;
-    }
-  }
-
-  .overwrite-label-note {
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 12px;
-    color: #6d7786;
-    a {
-      color: #6d7786;
-      font-weight: 400;
-      text-decoration: underline solid #6d7786 1.5px;
-    }
-  }
-  .overwrite-form-item-register {
-    width: 100%;
-    .el-input__inner,
-    .el-textarea__inner {
-      padding: 0.5rem 0.75rem;
-      background: #e7e9eb;
-      gap: 12px;
-      align-self: stretch;
-      border-color: #ced2d7;
-    }
-    .el-input-group__append {
-      border-left: none;
-      gap: 12px;
-      background: #e7e9eb;
-      border-color: #ced2d7;
-      font-weight: 600;
-      font-size: 18px;
-      line-height: 18px;
-      color: #3c4a5d;
-    }
-  }
-  .overwrite-radio-item {
-    background: #e7e9eb;
-    border-color: #ced2d7;
-    color: #0b1d35;
-  }
-
-  .check-box {
-    .el-checkbox {
-      .el-checkbox__label {
-        color: #3c4a5d;
-        font-size: 16px;
-      }
-      .el-checkbox__inner {
-        width: 24px;
-        height: 24px;
-        border-color: #ced2d7;
-        transition: none;
-      }
-      .is-checked {
-        .el-checkbox__inner {
-          background-color: #78c147;
-        }
-        .el-checkbox__inner::after {
-          border-bottom: 4px solid #fff;
-          border-right: 4px solid #fff;
-          transform: rotate(45deg);
-          font-size: 16px;
-          width: 5px;
-          height: 10px;
-          left: 7px;
-          bottom: 5px;
-        }
-      }
-    }
-  }
-  .el-button {
-    background: #a1d47e;
-    width: 100%;
-    font-size: 20px;
-    font-weight: 600;
-    color: #ffffff;
-  }
-}
+@import "./FromComponent.scss";
 </style>
